4dcd2e47f0ef4b79b2d35014dc57fa26dfdb7028
sm-harun <harunyuss12@gmail.com>
2024-08-09 20:08:37 +0300

    Initail commit

---
+++ src/lib/components/NavItems.svelte
@@ -0,0 +1,8 @@
+<nav class="list-nav">
+    <ul>
+        <li><a href="">Just a test 1</a></li>
+        <li><a href="">Lorem ipsum dolor sit amet.</a></li>
+        <li><a href="">Lorem, ipsum dolor.</a></li>
+        <li><a href="">Mama mia</a></li>
+    </ul>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { AppShell, AppBar, Modal, getModalStore, initializeStores } from '@skeletonlabs/skeleton';
+	import type { ModalComponent, ModalSettings, ModalStore } from '@skeletonlabs/skeleton';
+
+	import '../app.postcss';
+
+	import NavItems from '$lib/components/NavItems.svelte';
+
+	initializeStores();
+</script>
+
+<Modal />
+
+<AppShell slotPageHeader="h-20" slotSidebarLeft="bg-surface-500/50 w-52" slotSidebarRight="w-30 p-5 bg-surface-700">
+	<svelte:fragment slot="header">
+
+		<AppBar>
+			<h1 class="text-primary-500">Notes</h1>
+		</AppBar>
+
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+
+		<NavItems />
+
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">
+		<div class="btn bg-primary-500 grid place-items-center p-4">+Add</div>
+	</svelte:fragment>
+
+	<slot />
+
+</AppShell>
