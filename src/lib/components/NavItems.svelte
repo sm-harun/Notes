7e44508b740558c5ab13c07400721593e61bd5c8
sm-harun <harunyuss12@gmail.com>
2024-08-11 09:07:51 +0300

    Made it responsive for mobile

--- src/lib/components/NavItems.svelte
+++ src/lib/components/NavItems.svelte
@@ -1,6 +1,6 @@
 <nav class="list-nav p-3">
     <ul>
-        <li><a href="">Basic Notes App</a></li>
+        <li class="bg-primary-500/10 rounded-xl"><a href="">Basic Notes App</a></li>
         <li><a href=""></a></li>
         <li><a href="https://github.com/sm-harun">Made by: <p class="text-primary-500 ml-1"> SM-Harun</p></a></li>
         <li><a href="https://github.com/sm-harun/Notes">Github <p class="text-primary-500 ml-1">Repository</p></a></li>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { AppShell, AppBar, Modal, initializeStores, getModalStore, LightSwitch } from '@skeletonlabs/skeleton';
-	import type { ModalStore, ModalSettings } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, Modal, initializeStores, getModalStore, LightSwitch, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+	import type { ModalStore, ModalSettings, DrawerStore } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 
 	import NavItems from '$lib/components/NavItems.svelte';
@@ -10,6 +10,7 @@
 	initializeStores();
 	
 	const modalStore: ModalStore = getModalStore();
+	const drawerStore: DrawerStore = getDrawerStore();
 
 	function askPrompt() {
 		const modal: ModalSettings = {
@@ -33,20 +34,48 @@
 			}]
 		})
 	}
+
+	function openDrawer(): void {
+		drawerStore.open();
+	}
+
+	function closeDrawer(): void {
+		drawerStore.close();
+	}
 </script>
 
 <Modal />
 
-<AppShell slotHeader="h-50" slotSidebarLeft="bg-surface-500/20 w-52 p-1" slotSidebarRight="w-30 p-5 bg-surface-700">
+<Drawer width="w-60">
+	<div class="w-full h-full p-5">
+		<button on:click={closeDrawer}>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 hover:scale-110 focus:scale-110">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+			</svg>
+		</button>
+		<NavItems />
+	</div>
+</Drawer>
+
+<AppShell slotHeader="h-50" slotSidebarLeft="bg-surface-500/20 w-0 md:w-52 md:p-1" slotSidebarRight="bg-surface-700 hidden md:w-30 md:block md:p-5">
 	<svelte:fragment slot="header">
 
-		<AppBar>
+		<AppBar slotLead="md:hidden" slotTrail="ml-auto">
 			<svelte:fragment slot="lead">
-				<h1 class="text-primary-500">Notes</h1>
+				<button on:click={openDrawer}>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 focus:scale-110 hover:scale-110 transition">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+					</svg>
+				</button>
 			</svelte:fragment>
-
+			<strong>Notes</strong>
 			<svelte:fragment slot="trail">
 				<LightSwitch />
+				<button on:click={askPrompt}>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 md:hidden">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+					</svg>
+				</button>
 			</svelte:fragment>
 		</AppBar>
 
